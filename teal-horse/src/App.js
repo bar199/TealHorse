31310640b7101cd270e2d11eb4b4449b94b3133a
kmosoti <kennedy.mosoti@mavs.uta.edu>
2021-10-03 18:47:33 -0500

    Removed Excess Files - Added Rotating Head

--- teal-horse/src/App.js
+++ teal-horse/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import TealHorse from './images/TealHorse.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={TealHorse} className="App-logo" alt="logo"/>
+        Project Teal Horse
       </header>
     </div>
   );
